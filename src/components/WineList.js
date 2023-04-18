c16b6788b6cd8f371a4fea2599d6988582c4c0c8
Sarah Kuss <sarah.kuss33@gmail.com>
2023-04-18 11:11:46 -0400

    using useEffect and can now switch varieties

--- src/components/WineList.js
+++ src/components/WineList.js
@@ -1,22 +1,36 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import WineCard from "./WineCard"
 
 export default function WineList() {
   const [theWines, setTheWines] = useState() //no wines to put in here so leave it empty
+  const [color, setColor] = useState('reds')
   const getWines = () => {
-    fetch('https://api.sampleapis.com/wines/reds')
+    fetch(`https://api.sampleapis.com/wines/${color}`)
       .then(response => response.json())
       .then(data => setTheWines(data)) //.then(setTheWines)
       .catch(alert)
   }
+
+  useEffect(() => {
+    getWines()
+  }, [color]) //if color changes get the wines onClick={() => setColor('')}
+
   return (
-    <section className="wine-list">
-      {(!theWines)
-        ? <button onClick={getWines}>Get Wine List</button>
-        : theWines.map(wine => (
-          <WineCard key={wine.id} wine={wine} /> 
-        ))
-      }
-    </section>
+    <>
+      <div className="buttons">
+        <button onClick={() => setColor('reds')}>Reds</button>
+        <button onClick={() => setColor('whites')}>Whites</button>
+        <button onClick={() => setColor('sparkling')}>Sparkling</button>
+        <button onClick={() => setColor('rose')}>Rose</button>
+      </div>
+      <section className="wine-list">
+        {(!theWines)
+          ? <h2>Loading...</h2>
+          : theWines.map(wine => (
+            <WineCard key={wine.id} wine={wine} /> 
+          ))
+        }
+      </section>
+    </>
   )
 }
